858ac178291b7d3e835a248fcb21cd14fb03cb31
Robson Jean Penteado <robjean9@hotmail.com>
2020-09-28 18:16:35 -0300

    add validation on update ocurrence

--- backend/src/routes/ocurrence.js
+++ backend/src/routes/ocurrence.js
@@ -76,8 +76,38 @@ router.delete('/:id',
 
 router.put('/:id',
     middleware.middleware,
+    [
+      body('description').isString().isLength({min: 1}),
+      body('type').isString().isIn(['assalto',
+      'agressao',
+      'covid',
+      'perturbacao',
+      'homicidio',
+      'atividade_suspeita',
+      'acidente',
+      'desaparecimento',
+      'animal_perdido']),
+      body('zip_code').isString().isLength({min: 1}),
+      body('city').isString().isLength({min: 1}),
+      body('ocurred_at').isNumeric(),
+      body('neighborhood').isString().isLength({min: 1}),
+      body('street').isString().isLength({min: 1}),
+      body('number').optional().isNumeric(),
+      body('latitude').isNumeric(),
+      body('longitude').isNumeric(),
+      body('anonymous').isBoolean()
+    ],
     (req,res)=>{
-      ocurrenceController.update(req,res)
+      if(!validationResult(req).isEmpty()){
+        console.log(+ validationResult(req).mapped());  
+        console.log(validationResult(req));  
+        let errors  = [];
+        await validationResult(req).array({onlyFirstError:true}).map((item=>  errors.push(item.param)));
+        errors = errors.join(', ');
+        return res.status(400).send({error:`Campos obrigatórios não preenchidos: ${errors}` });
+      }else{
+        ocurrenceController.update(req,res)
+      }
     }
 )
 
